945e1a0c66f5696f79a5d4d31b36bc72b8f98fc9
ajadiolatunde <ajadiolatunde@gmail.com>
2019-09-02 16:03:22 +0100

    add more test cases

--- shop/tests.py
+++ shop/tests.py
@@ -1,3 +1,18 @@
 from django.test import TestCase
+from .models import Category,Product
 
 # Create your tests here.
+# https://scotch.io/tutorials/build-a-rest-api-with-django-a-test-driven-approach-part-1
+class ModelTestCase(TestCase):
+    """THis define test list for category and Product"""
+    def setUp(self):
+        self.category = Category(name='People',slug='people')
+    
+
+    def test_model_can_create_category(self):
+
+        """MODE L can create Category"""
+        old_count = Category.objects.count()
+        self.category.save()
+        new_count = Category.objects.count()
+        self.assertNotEquals(old_count,new_count)
